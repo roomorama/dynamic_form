b7d63630161116be65b2ea2ab38f1adf4082c0bd
Donald Piret <donald@donaldpiret.com>
2013-01-08 16:13:56 +0800

    Update lib/active_model/dynamic_errors.rb
    Comment out the custom full_messages function

--- lib/active_model/dynamic_errors.rb
+++ lib/active_model/dynamic_errors.rb
@@ -10,36 +10,36 @@ module ActiveModel
     #     :message => Proc.new { |aa| "#{aa.label} (#{aa.group_label}) is required" }
     #     which gives an error message like:
     #     Rate (Accuracy) is required
-    def full_messages
-      full_messages = []
-
-      each do |attribute, messages|
-        messages = Array.wrap(messages)
-        next if messages.empty?
-
-        if attribute == :base
-          messages.each {|m| full_messages << m }
-        else
-          attr_name = attribute.to_s.gsub('.', '_').humanize
-          attr_name = @base.class.human_attribute_name(attribute, :default => attr_name)
-          options = { :default => "%{attribute} %{message}", :attribute => attr_name }
-
-          messages.each do |m|
-            if m =~ /^\^/
-              options[:default] = "%{message}"
-              full_messages << I18n.t(:"errors.dynamic_format", options.merge(:message => m[1..-1]))
-            elsif m.is_a? Proc
-              options[:default] = "%{message}"
-              full_messages << I18n.t(:"errors.dynamic_format", options.merge(:message => m.call(@base)))
-            else
-              full_messages << I18n.t(:"errors.format", options.merge(:message => m))
-            end            
-          end
-        end
-      end
-
-      full_messages
-    end
+    #def full_messages
+    #  full_messages = []
+    #
+    #  each do |attribute, messages|
+    #    messages = Array.wrap(messages)
+    #    next if messages.empty?
+    #
+    #    if attribute == :base
+    #      messages.each {|m| full_messages << m }
+    #    else
+    #      attr_name = attribute.to_s.gsub('.', '_').humanize
+    #      attr_name = @base.class.human_attribute_name(attribute, :default => attr_name)
+    #      options = { :default => "%{attribute} %{message}", :attribute => attr_name }
+    #
+    #      messages.each do |m|
+    #        if m =~ /^\^/
+    #          options[:default] = "%{message}"
+    #          full_messages << I18n.t(:"errors.dynamic_format", options.merge(:message => m[1..-1]))
+    #        elsif m.is_a? Proc
+    #          options[:default] = "%{message}"
+    #          full_messages << I18n.t(:"errors.dynamic_format", options.merge(:message => m.call(@base)))
+    #        else
+    #          full_messages << I18n.t(:"errors.format", options.merge(:message => m))
+    #        end            
+    #      end
+    #    end
+    #  end
+    #
+    #  full_messages
+    #end
   end
 end
 
